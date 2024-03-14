13852d24e328d39fa6c3858b8564c1ccde55a271
shreshtsharma <sharmashresht7@gmail.com>
2024-03-14 18:55:49 +0530

    New keyword

---
+++ 10_classes_and_oop/OOP.js
@@ -0,0 +1,45 @@
+
+// Object Literal
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+console.log(user.getUserDetails());
+console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+
+// new is the constructor function here 
+// it gives the new instance of User to the object 
+// so that no one can overwrite any value of each other objects
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(typeof userOne)
+//console.log(userTwo);
